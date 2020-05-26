f48d408769375ea70dca4f53957725621d003559
Steve McGuinness <steve.mcguinness@gmail.com>
2020-05-26 16:15:27 -0400

    Initial project setup

---
+++ SwiftyLibSAM/SwiftyLibSAM.h
@@ -0,0 +1,19 @@
+//
+//  SwiftyLibSAM.h
+//  SwiftyLibSAM
+//
+//  Created by Steve Mcguinness on 5/26/20.
+//  Copyright © 2020 Steve Mcguinness. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftyLibSAM.
+FOUNDATION_EXPORT double SwiftyLibSAMVersionNumber;
+
+//! Project version string for SwiftyLibSAM.
+FOUNDATION_EXPORT const unsigned char SwiftyLibSAMVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyLibSAM/PublicHeader.h>
+
+
---
+++ SwiftyLibSAMExamples/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftyLibSAMExamples
+//
+//  Created by Steve Mcguinness on 5/26/20.
+//  Copyright © 2020 Steve Mcguinness. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
